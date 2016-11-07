16c9240d8bd78f0077cac7fc4738b7963302eeb8
pjfl <github@roxsoft.co.uk>
2016-11-07 02:49:05 +0000

    Prefer template layout to page layout

--- lib/Web/Components/Role/TT.pm
+++ lib/Web/Components/Role/TT.pm
@@ -2,7 +2,7 @@ package Web::Components::Role::TT;
 
 use 5.010001;
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.7.%d', q$Rev: 1 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.7.%d', q$Rev: 2 $ =~ /\d+/gmx );
 
 use File::DataClass::Constants qw( EXCEPTION_CLASS NUL TRUE );
 use File::DataClass::Types     qw( Directory Object );
@@ -48,7 +48,7 @@ my $_layout = sub {
 
    my $page   = $stash->{page} //= {};
    my $plate  = $stash->{template} //= {};
-   my $layout = $page->{layout} // $plate->{layout} // $conf->layout;
+   my $layout = $plate->{layout} // $page->{layout} // $conf->layout;
 
    return $plate->{layout} = $page->{layout} = $layout;
 };
